67db6ea7047c947e21afb080809ca4e5cb1dbf3e
mhipereyre <62816876+mirthes@users.noreply.github.com>
2020-03-30 18:12:41 -0300

    Add files via upload

---
+++ man.js
@@ -0,0 +1,64 @@
+
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*var count = 0;
+while (count < 5) {
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{name:"maça", cor:"vermelha"}, {name:"uva", cor: "roxo"}]
+console.log(frutas);
+alert(frutas[1].name);
+*/
+
+/*
+var fruta = {name:"maça", cor:"vermelha"};
+console.log(fruta.name);
+alert(fruta.cor);
+*/
+
+/*
+var liste = ["Maça", "pêra", "laranja"];
+console.log(liste[0]);
+console.log(liste.toString()[0]);
+console.log(liste.join(" - "));
+//console.log(liste.reverse());
+//liste.pop();
+//liste.push("uva");
+//console.log(liste.length);
+//console.log(liste[2]);
+//alert(liste[1]);
+*/
+
+/*
+var name = "Mhipereyre";
+var n = 3;
+var n2 = 5;
+var frase = "Barcelona é o melhor time do mundo";
+//alert( name + "tem" + idade + "anos");
+//alert(idade + idade2)
+console.log(name);
+console.log(n * n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Barcelona", "Real"));
+*/
