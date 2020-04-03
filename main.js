eb95ba61d8dc5677328aca8238230b5d579039cf
mhipereyre <62816876+mirthes@users.noreply.github.com>
2020-04-03 19:38:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,145 @@
+/*
+ var x = 10;
+ var y = 5;
+ var resultado = x + y;
+   console.log(resultado);
+   */
+ //console.log(x, y)
+
+/*var name = "Mirthes";
+var idade = 30;
+var peso = 115;
+var humano = true;
+var humano = false;
+var alunos =  ["LavYnia", "Vanessa"];
+var alunos = {
+   nome: "Mirthes",
+   idade: 30,
+   peso: 115,
+   humano: true
+  };
+console.log(alunos);
+*/
+
+/*function clicked(){
+    document.getElementById("acknowledgmant").innerHTML = "thanks for clicking";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("thanks for clicking!");
+}
+
+function redirect() {
+    window.open("https://enem.inep.gov.br/");
+    window.location.href = "https://enem.inep.gov.br/";
+
+}
+
+function trocar() {
+    elemento.innerHTML = "tkanks for hovering";
+    //document.getElementById("elemento").innerHTML = "tkanks for hovering";
+    //alert("trocar texto");
+}
+
+function voltar(element) {
+    elemento.innerHTML = "mouse over here";
+    //document.getElementById("elemento").innerHTML = "mouse over here";
+}
+
+function load(element) {
+    alert("page loaded");
+}
+
+function functionchange(element){
+    console.log(element.value);
+}
+*/
+
+/*function soma(n, n1) {
+    return n - n1;
+}
+
+function validaIdade(idade) {
+    var validor;
+    if (idade >= 18){
+        validor = true;
+    }else{
+        validor = false
+    }
+    return validor;
+}
+
+var idade = prompt("qual sua idade?");
+console.log(validaIdade(idade));
+*/
+
+/*function soma(n, n1) {
+    return n - n1;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert (soma(5, 10));
+alert(setReplace("vai japão", "japão", "brasil"));
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*var count = 0;
+while (count < 5) {
+    console.log(count);
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual sua idade");
+//var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{name:"maça", cor:"vermelha"}, {name:"uva", cor: "roxo"}]
+console.log(frutas);
+alert(frutas[1].name);
+*/
+
+/*
+var fruta = {name:"maça", cor:"vermelha"};
+console.log(fruta.name);
+alert(fruta.cor);
+*/
+
+/*
+var liste = ["Maça", "pêra", "laranja"];
+console.log(liste[0]);
+console.log(liste.toString()[0]);
+console.log(liste.join(" - "));
+//console.log(liste.reverse());
+//liste.pop();
+//liste.push("uva");
+//console.log(liste.length);
+//console.log(liste[2]);
+//alert(liste[1]);
+*/
+
+/*var name = "Mhipereyre";
+var n = 3;
+var n2 = 5;
+var frase = "Barcelona é o melhor time do mundo";
+//alert( name + "tem" + idade + "anos");
+//alert(idade + idade2)
+console.log(name);
+console.log(n * n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Barcelona", "Real"));
+*/
